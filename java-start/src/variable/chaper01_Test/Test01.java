f763d8ac6c9f30f869b5958eac4eab38030d9b0a
namhosung6023 <hosung60023@naver.com>
2024-01-02 16:56:51 +0900

    operator

---
+++ java-start/src/variable/chaper01_Test/Test01.java
@@ -0,0 +1,10 @@
+package variable.chaper01_Test;
+
+public class Test01 {
+    public static void main(String[] args) {
+        int num1 = 4, num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
